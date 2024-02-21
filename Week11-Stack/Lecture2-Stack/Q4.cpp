a36974987c04186781d28d02cd16a8896a046f3e
Khushal Ravindra Bhavsar <114830517+Khushal41@users.noreply.github.com>
2024-02-22 00:03:52 +0530

    Initial Commit

---
+++ Week11-Stack/Lecture2-Stack/Q4.cpp
@@ -0,0 +1,65 @@
+// Sort a stack 
+#include <iostream>
+#include <stack>
+using namespace std;
+
+void insertSorted(stack<int> &s, int target)
+{
+    // base case
+    if (s.empty())
+    {
+        s.push(target);
+        return;
+    }
+    // Top element greater than equal to target to push
+    if (s.top() >= target)
+    {
+        s.push(target);
+        return;
+    }
+
+    int topElement = s.top();
+    s.pop();
+    // Recursive call
+    insertSorted(s, target);
+
+    // Backtraking
+    s.push(topElement);
+}
+
+void sortStack(stack<int> &s)
+{
+    // base case
+    if (s.empty())
+    {
+        return;
+    }
+
+    int topElement = s.top();
+    s.pop();
+
+    sortStack(s);
+
+    insertSorted(s, topElement);
+}
+
+int main()
+{
+    stack<int> s;
+    s.push(7);
+    s.push(11);
+    s.push(3);
+    s.push(5);
+    s.push(9);
+
+    sortStack(s);
+
+    cout << "Printing" << endl;
+    while (!s.empty())
+    {
+        cout << s.top() << " ";
+        s.pop();
+    }
+    cout << endl;
+    return 0;
+}
