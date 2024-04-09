147eefd9d90e115913d0cd953a5b5c3e57d49450
Khushal Ravindra Bhavsar <114830517+Khushal41@users.noreply.github.com>
2024-04-09 22:48:26 +0530

    Initial Commit

--- Week13-Trees/Lecture1-Trees/Code2.cpp
+++ Week13-Trees/Lecture1-Trees/Code2.cpp
@@ -9,31 +9,28 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution {
+class Solution
+{
 public:
-    int maxDepth(TreeNode* root) {
-        if(root == NULL) 
-		return 0;
+    int maxDepth(TreeNode *root)
+    {
+        if (root == NULL)
+            return 0;
 
         int leftHeight = maxDepth(root->left);
         int rightHeight = maxDepth(root->right);
         int ans = max(leftHeight, rightHeight) + 1;
         return ans;
     }
-    int diameterOfBinaryTree(TreeNode* root) {
-        if(root == NULL) 
-        return 0;
+    int diameterOfBinaryTree(TreeNode *root)
+    {
+        if (root == NULL)
+            return 0;
 
         int op1 = diameterOfBinaryTree(root->left);
         int op2 = diameterOfBinaryTree(root->right);
         int op3 = maxDepth(root->left) + 1 + maxDepth(root->right);
         int ans = max(op1, max(op2, op3));
-        return ans ;
+        return ans;
     }
-
-
-
-
-
-
 };
